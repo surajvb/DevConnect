1a3fdf6426ed432b51ea1b6344cc84768468e4c5
Suraj Veerappa Bolanavar <SBolanavar@synamedia.com>
2020-03-25 17:57:40 +0530

    Adding a middleware and get the authorised user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
+  }
+});
 
 module.exports = router;

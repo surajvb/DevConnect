756cecd310ca409bc8b153df6ea878cb64f93fdd
Suraj Veerappa Bolanavar <SBolanavar@synamedia.com>
2020-03-25 16:44:57 +0530

    First successful save to mongo via API from postman
    POST api : http://localhost:5000/api/users
    and the body is :
    {
    	"name":"suraj",
    	"email":"svbsuraj@gmail.com",
    	"password":"123456"
    }

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -17,13 +22,43 @@ router.post(
       'please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('User route');
+    const { name, email, password } = req.body;
+
+    try {
+      // see if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // Return jsonwebtoken
+      res.send('User registered');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
